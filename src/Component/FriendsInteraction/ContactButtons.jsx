a3298cfc6cb4c090c02a0673331163dcde166f35
afia-labonno <afia.cse01@gmail.com>
2026-04-26 20:01:58 +0600

    save contact history with localstorage and display on TimeLine Page.

--- src/Component/FriendsInteraction/ContactButtons.jsx
+++ src/Component/FriendsInteraction/ContactButtons.jsx
@@ -3,11 +3,27 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 
+
 const ContactButtons = ({type, friend}) => {
 
-     const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    // Contact History save
+    const saveToTimeline = (newContactInfo) =>{
+        const existing = JSON.parse(localStorage.getItem("timeline"))||[] ;
+        localStorage.setItem("timeline", JSON.stringify([newContactInfo, ...existing])) ;
+    }
 
     const handleContact = ()=>{
+
+        const newContactInfo = {
+            id: Date.now(),
+            type,
+            name: friend.name,
+            date: new Date().toISOString()
+    };
+        saveToTimeline(newContactInfo) ;
+
         // console.log(type, 'clicked')
         // logic:
        setLoading(true);
@@ -20,7 +36,7 @@ const ContactButtons = ({type, friend}) => {
             message = `Text with ${friend.name}`
        }
        else if(type === "video"){
-            message = `Video with ${friend.name}`
+            message = `Video call with ${friend.name}`
        }
 
        setTimeout(()=>{
@@ -35,7 +51,7 @@ const ContactButtons = ({type, friend}) => {
     return (
         <button onClick={handleContact}
             className="text-lg btn btn-ghost text-gray-700" disabled={loading}>
-            {type}
+            {loading ? "..." : type}
         </button>
     );
 };
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,16 +1,122 @@
-
-
-export const metadata = {
-  title: "Timeline Page | keenKeepr ",
-  description: "Contact history",
-};
+'use client'
+import { useEffect, useState } from "react";
 
 
 const TimelinePage = () => {
+    const [logs, setLogs] = useState([]);
+    const [filter, setFilter] = useState("all");
+    const [search, setSearch] = useState('');
+
+    // Data Loading
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem("timeline")) || [];
+        setLogs(data);
+
+    }, []);
+
+    const filteredLogs = logs.filter((log) => {
+        //filter type
+        const matchType = filter === "all" || log.type === filter
+
+        const matchSearch = log.name.toLowerCase().includes(search.toLocaleLowerCase());
+
+        return matchType && matchSearch
+    });
+
     return (
-        <div>
-            <h2 className="text-7xl">Timeline Page</h2>
+        <div className="container mx-auto space-y-4 lg:px-[120px] py-20">
+
+            <div className="flex justify-between">
+                <div>
+                    <h2 className="text-3xl font-bold text-green-950 mb-6">
+                        Timeline
+                    </h2>
+                </div>
+
+                <div className="flex gap-4">
+
+                     {/* sort by */}
+                    <div>
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn m-1">Sort By ⬇️</div>
+                            <ul
+                                tabIndex="-1"
+                                className="menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-52 p-2 shadow-sm">
+                                <li>
+                                    <button onClick={() => setFilter("all")} className="btn btn-sm">
+                                        All
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setFilter("call")} className="btn btn-sm">
+                                        Call
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setFilter("text")} className="btn btn-sm">
+                                        Text
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setFilter("video")} className="btn btn-sm">
+                                        Video
+                                    </button>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* search field */}
+                    <div>
+                        
+                        <input onChange={(e) => setSearch(e.target.value)}
+                            type="text" placeholder="Search" value={search}
+                            className="border border-base-200 shadow-md p-4"
+                        />
+                    </div>
+                   
+                </div>
+            </div>
+
+
+
+
+            {/* contact History list */}
+            <div className="space-y-4 max-w-7xl">
+                {
+                    filteredLogs.length > 0 ? (
+                        filteredLogs.map((log) =>
+                            <div key={log.id}
+                                className="shadow-md border border-base-200 rounded-md px-6 py-4">
+
+                                <p className="font-semibold">
+                                    {log.type} with {log.name}
+                                </p>
+
+                                <p className="text-sm text-gray-500">
+                                    {new Date(log.date).toLocaleString()}
+                                </p>
+                            </div>
+                        )
+                    ) : (
+                        <div className=" max-w-5xl max-auto border border-base-200 shadow-md p-8 rounded-md text-center bg-gray-50">
+                            <h3 className="text-lg font-semibold text-gray-600">
+                                No interactions found
+                            </h3>
+                            <p className="text-sm text-gray-500 mt-2">
+                                Try different filter or search
+                            </p>
+                        </div>
+                    )
+                }
+
+            </div>
+
         </div>
+
+
+
     );
 };
 
